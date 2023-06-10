220c46404a840e5acc26984a839099dadb218ad0
Hasibur Rahman <hasiburdev@gmail.com>
2023-06-10 14:59:23 +0600

    Update product-card.component.ts

--- src/app/pages/home/components/product-card/product-card.component.ts
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
-
+  @Input() fullWidthMode = false;
 }
